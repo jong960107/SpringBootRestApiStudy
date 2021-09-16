4dcbd3ae7a297b34f26ee3508f13696dd24c27a1
ovdncids <ovdncids@nate.com>
2021-09-17 08:04:01 +0900

    [#1]Swagger UI 설정

--- src/main/java/com/example/SpringBootRestApi/SpringBootRestApiApplication.java
+++ src/main/java/com/example/SpringBootRestApi/SpringBootRestApiApplication.java
@@ -2,6 +2,13 @@ package com.example.SpringBootRestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class SpringBootRestApiApplication {
@@ -10,4 +17,13 @@ public class SpringBootRestApiApplication {
 		SpringApplication.run(SpringBootRestApiApplication.class, args);
 	}
 
+	@Configuration
+	@EnableSwagger2
+	public class SwaggerConfig {
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any()).build();
+		}
+	}
 }
